0f2c59ef18a50c053802938d6a47391589303434
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-09-07 15:41:00 +0200

    [ADD] - decorators, filters, interceptors, formatter

--- apps/tellme-back/src/main.ts
+++ apps/tellme-back/src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PING, PONG } from '@tellme/core';
-
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter, ResponseInterceptor } from '@tellme/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(PING) 
-  console.log(PONG)
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(helmet());
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Removes properties not defined in the DTO
+      forbidNonWhitelisted: true, // Rejects the request if non-whitelisted properties are present
+      transform: true, // Transforms payloads into class instances
+      transformOptions: {
+        enableImplicitConversion: true, // Enables automatic type conversion
+      },
+    }),
+  );
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  const port = process.env.PORT ?? 3002;
+  await app.listen(port);
+  console.log(`Tellme Backend listening on port ${port}.`);
 }
 bootstrap();
